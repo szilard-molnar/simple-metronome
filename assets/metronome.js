e971adbe234637f977bec85db3d62f6c6227b2e4
szilard-molnar <mszilard86@gmail.com>
2020-06-07 16:49:50 -0400

    meter and tempo values are getting stored into their variables

--- assets/metronome.js
+++ assets/metronome.js
@@ -1,3 +1,25 @@
 let meter;
 let tempo;
 
+function grabMeter() {
+    if(document.getElementById("three").checked)
+    {
+        meter = document.getElementById("three").value;
+        console.log(meter);
+    }
+    else if(document.getElementById("four").checked)
+    {
+        meter = document.getElementById("four").value;
+        console.log(meter);
+    }    
+}
+
+function grabTempo() {
+    tempo = document.getElementById("tempo").value;
+    console.log(tempo);
+}
+
+function clickButton() {
+    grabMeter();
+    grabTempo();
+}
